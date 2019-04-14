71b52556bbd82ee8457527e77a102b5a303d6c97
liweitao92 <532533603@qq.com>
2019-04-14 10:05:00 +0800

    新增权限

--- src/api/permission.js
+++ src/api/permission.js
@@ -11,3 +11,16 @@ export function getPermissionPager (params) {
     params
   })
 }
+
+/**
+ * 创建权限信息
+ * @param data
+ * @returns {never}
+ */
+export function createPermission (data) {
+  return request({
+    url: '/permission',
+    method: 'post',
+    data
+  })
+}
